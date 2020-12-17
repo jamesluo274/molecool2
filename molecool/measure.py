4a2598139d501ba75c55e1867df1f490064213ef
Tengfei Luo <tluo@nd.edu>
2020-12-17 14:15:38 -0500

    run black on modules

--- molecool/measure.py
+++ molecool/measure.py
@@ -4,16 +4,17 @@ This module is for functions which perform measurements.
 
 import numpy as np
 
+
 def calculate_distance(rA, rB):
     """
     Calculates the distance between two points.
 
     Parameters
     ----------
-    rA : np.ndarray 
-        The coordinate of point A 
-    
-    rB : np.ndarray 
+    rA : np.ndarray
+        The coordinate of point A
+
+    rB : np.ndarray
         The coordinate of point B
 
     Retruns
@@ -30,16 +31,17 @@ def calculate_distance(rA, rB):
     """
 
     # This function calculates the distance between two points given as numpy arrays.
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
+    d = rA - rB
+    dist = np.linalg.norm(d)
     return dist
-    
+
+
 def calculate_angle(rA, rB, rC, degrees=False):
     # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
     # by setting degrees=True
     AB = rB - rA
     BC = rB - rC
-    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
+    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
 
     if degrees:
         return np.degrees(theta)
