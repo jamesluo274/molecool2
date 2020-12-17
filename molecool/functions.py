a53da94ffbdcd2325b7d06513eeee94305db3c1b
Tengfei Luo <tluo@nd.edu>
2020-12-17 14:22:06 -0500

    fix imports on modules

--- molecool/functions.py
+++ molecool/functions.py
@@ -10,8 +10,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from mpl_toolkits.mplot3d import Axes3D
-      
-      
+
+
 def canvas(with_attribution=True):
     """
     Placeholder function to show example docstring (NumPy format)
