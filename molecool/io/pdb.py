db60ce3a33a2f29046adff2a83376ad4267d5d67
Tengfei Luo <tluo@nd.edu>
2020-12-17 21:19:14 -0500

    cleaned up pdb.py in IO using black

--- molecool/io/pdb.py
+++ molecool/io/pdb.py
@@ -13,11 +13,11 @@ def open_pdb(f_loc):
     c = []
     sym = []
     for l in data:
-        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
+        if "ATOM" in l[0:6] or "HETATM" in l[0:6]:
             sym.append(l[76:79].strip())
             c2 = [float(x) for x in l[30:55].split()]
             c.append(c2)
 
     coords = np.array(c)
-    
+
     return sym, coords
